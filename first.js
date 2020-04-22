6fe3b12949840d01a7630ed7c2db371399687346
Gabriel <49006461+bihellzin@users.noreply.github.com>
2020-04-21 21:32:43 -0300

    Create first.js

---
+++ first.js
@@ -0,0 +1,46 @@
+function setup() {
+  createCanvas(1280, 640);
+  background(0);
+}
+
+
+function draw() {  
+  
+  if(mouseIsPressed){
+    setup();
+    arr = shuff(createArray());
+  
+    for (let i = 1; i <= 1280; i++){
+      ellipse(i, arr[i-1], 3, arr[i-1]);
+    }
+  } 
+}
+
+
+function createArray(){
+  arr = []
+  for (let i = 1; i <= 1280; i++){
+   arr.push(i)
+ }
+  return arr
+}
+
+function shuff(array) {
+  let currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
